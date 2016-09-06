5bc1ea4303519e90f20230d455de3eeabc87d8e0
Pc <Pc@DESKTOP-2VUBUTH>
2016-09-05 22:23:21 -0500

    commit5

---
+++ Commit1/src/commit1/Commit5.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commit1;
+
+/**
+ *
+ * @author Pc
+ */
+public class Commit5 {
+           public static void main(String[] args) {
+        int a= 60;
+        int b=45;
+        int c= a+b;
+        System.out.println(" Rasultado " +c);
+               System.out.println("Ultimo Commit");
+    }
+}
