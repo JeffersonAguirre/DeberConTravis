8bf95994d0e2fce13c6c56247680cec6cfc6ea39
Pc <Pc@DESKTOP-2VUBUTH>
2016-09-05 22:21:14 -0500

    commit2

---
+++ Commit1/src/commit1/Commit2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commit1;
+
+/**
+ *
+ * @author Pc
+ */
+public class Commit2 {
+       public static void main(String[] args) {
+        int a= 2;
+        int b=3;
+        int c= a+b;
+        System.out.println(" Rasultado " +c);
+    }
+}
